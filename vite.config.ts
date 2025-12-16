f9027b36a411f89d1cc3aefda25af1d0b494ebd5
Zhang Wentao <13064145510@wo.cn>
2025-12-16 22:21:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -84,7 +84,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:5244",
+        target: "https://alist.xiaoya.pro",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
